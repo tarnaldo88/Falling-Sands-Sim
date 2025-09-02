90ade0033344fa561f0d15995c4c908381c6bf60
tarnaldo88 <tarnaldo88@gmail.com>
2025-09-01 19:33:48 -0700

    starting with basic function to make a 2d array

---
+++ FallingSand.js
@@ -0,0 +1,9 @@
+//Falling sand simulator
+
+function make2DArray(cols, rows){
+    let arr = new Array(cols);
+    for(let i = 0; i < arr.length; i++){
+        arr[i] = new Array(rows);
+    }
+    return arr;
+}
