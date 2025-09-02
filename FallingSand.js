2ab056f41898735ad01c7dbf17dea4ee83735508
tarnaldo88 <tarnaldo88@gmail.com>
2025-09-01 19:35:50 -0700

    setting up width of canvas

--- FallingSand.js
+++ FallingSand.js
@@ -7,3 +7,18 @@ function make2DArray(cols, rows){
     }
     return arr;
 }
+
+let grid;
+let w = 10;
+let cols, rows;
+
+function setup(){
+    createCanvas(400,400);
+    cols = width / w;
+    rows = height / w;;
+    grid = make2DArray(cols, rows);
+}
+
+function draw(){
+    background(220);
+}
