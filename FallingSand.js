00f8af88d8fa6d92880a3d274c6096d0aa702bb9
tarnaldo88 <tarnaldo88@gmail.com>
2025-09-01 19:45:27 -0700

    adding loop to check the cells

--- FallingSand.js
+++ FallingSand.js
@@ -17,8 +17,61 @@ function setup(){
     cols = width / w;
     rows = height / w;;
     grid = make2DArray(cols, rows);
+
+    //set every square to 0
+    for(let i = 0; i < cols; i++){
+        for(let j = 0; j < rows; j++){
+            grid[i][j] = 0;
+        }
+    }
 }
 
 function draw(){
-    background(220);
+    background(0);
+
+    //set every square to 0
+    for(let i = 0; i < cols; i++){
+        for(let j = 0; j < rows; j++){
+            stroke(255);
+
+            //if value is 1 then fill will be white (1*255)
+            fill(grid[i][j]*255);
+            let x = i * w;
+            let y = j * w;
+            square(x,y,w);
+        }
+    }
+
+    // Create a 2D array for the next frame of animation
+    let nextGrid = make2DArray(cols, rows);
+  
+    // Check every cell
+    for (let i = 0; i < cols; i++) {
+        for (let j = 0; j < rows ; j++) {
+        // What is the state?
+        let state = grid[i][j];
+        
+            // If it's a piece of sand!
+            if (state > 0) {
+                // What is below?
+                let below = grid[i][j + 1];
+                
+                // Randomly fall left or right
+                let dir = 1;
+                if (random(1) < 0.5) {
+                dir *= -1;
+                }
+                
+                // Check below left or right
+                let belowA = -1;
+                let belowB = -1;
+                if (withinCols(i + dir)) {
+                belowA = grid[i + dir][j + 1];
+                }
+                if (withinCols(i - dir)) {
+                belowB = grid[i - dir][j + 1];
+                }
+            }
+        }
+    }
 }
